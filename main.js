42fe065a90b75682f5178616a86a134eda3ccbf7
Anna Kuś <aniaa.kus@gmail.com>
2022-08-28 18:41:24 +0200

    Add events

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("test")
+let seoContent = document.querySelector("#seo-content");
+let checkBtn = document.querySelector("#check-btn");
+let inputKeyword = document.querySelector(".inputkeywords");
+let inputKeywordBtn = document.querySelector("#input-keyword-btn");
+let keywordsList = document.querySelector(".keywords-list");
+let resetKeywordsBtn = document.querySelector("#reset-keywords-btn");
+let itemList = [];
+
+inputKeywordBtn.addEventListener("click", addKeyword());
+
+function addKeyword() {
+    let value = inputKeyword.value;
+    itemList.push(value);
+    inputKeyword.value = "";
+}
+
+
+console.log(itemList);
+
+
+
+
