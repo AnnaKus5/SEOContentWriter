87d4b217542df8175f239fa08caea14efde30666
Anna Kuś <aniaa.kus@gmail.com>
2022-09-11 20:24:38 +0200

    Add check keyword feature

--- main.js
+++ main.js
@@ -10,27 +10,32 @@ inputKeywordBtn.addEventListener("click", addKeyword);
 
 function addKeyword() {
     //możliwość dodania pustego stringa - poprawić
-    let value = {
-        name: inputKeyword.value,
-        frequency: 0
+    if (inputKeyword.value !== "") {
+        let value = {
+            name: inputKeyword.value,
+            frequency: 0
+        }
+        itemList.push(value);
+        addToList(value);
+        inputKeyword.value = "";
     }
-    itemList.push(value);
-    addToList(value);
-    inputKeyword.value = "";
+    // else {
+
+    // }
+
 }
 
 
 function addToList(value) {
     let li = document.createElement("li");
     li.classList.add("keyword-item");
-    li.innerText = value.name;
     let frequencyNumber = document.createElement("span");
     frequencyNumber.classList.add("keyword-frequency");
+    li.innerText = value.name;
     frequencyNumber.innerText = value.frequency;
     li.append(frequencyNumber);
     keywordsList.append(li);
 }
-
 
 resetKeywordsBtn.addEventListener("click", removeKeywords);
 
@@ -44,21 +49,26 @@ checkBtn.addEventListener("click", findKeywords);
 function findKeywords() {
     itemList.forEach(el => {
         el.indexValue = createListOfIndex(el.name, seoContent.value);
-        console.log(el.indexValue);
         el.frequency = el.indexValue.length;
     })
 
     function createListOfIndex (value, source) {
-
         let localIndexArray = [];
         let index = -1;
-
         while ((index=source.indexOf(value, index + 1)) >= 0) {
             localIndexArray.push([index, index + value.length]);
         }
-
         return localIndexArray;
+    }
 
+    updateFrequency(itemList);
+
+    function updateFrequency(list) {
+        let listOfValue = document.querySelectorAll(".keyword-frequency");
+        for (let i = 0; i < listOfValue.length; i++) {
+            let keywordFrequency = listOfValue[i];
+            keywordFrequency.innerText = list[i].frequency;
+        }  
     }
 
 }
