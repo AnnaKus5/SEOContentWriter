20daff16a71762323b7c02533948bd0554a14992
Anna Kuś <aniaa.kus@gmail.com>
2022-10-28 19:14:13 +0200

    Cleaning the code

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-let seoContent = document.querySelector("#seo-content");
-let fakeTextarea = document.querySelector(".fake-textarea");
-let checkBtn = document.querySelector("#check-btn");
-let inputKeyword = document.querySelector(".inputkeywords");
-let inputKeywordBtn = document.querySelector("#input-keyword-btn");
-let keywordsList = document.querySelector(".keywords-list");
-let resetKeywordsBtn = document.querySelector("#reset-keywords-btn");
-let itemList = [];
+const seoContent = document.querySelector("#seo-content");
+const fakeTextarea = document.querySelector("#fake-textarea");
+const checkBtn = document.querySelector("#check-btn");
+const inputKeyword = document.querySelector("#inputkeywords");
+const inputKeywordBtn = document.querySelector("#input-keyword-btn");
+const keywordsList = document.querySelector("#keywords-list");
+const resetKeywordsBtn = document.querySelector("#reset-keywords-btn");
+const itemList = [];
 
 inputKeywordBtn.addEventListener("click", addKeyword);
 resetKeywordsBtn.addEventListener("click", removeKeywords);
 checkBtn.addEventListener("click", findKeywords);
-
 
 function addKeyword() {
     if (inputKeyword.value !== "") {
@@ -22,19 +21,18 @@ function addKeyword() {
         addToList(value);
         inputKeyword.value = "";
     }
-
-    function addToList(value) {
-        let li = document.createElement("li");
-        li.classList.add("keyword-item");
-        let frequencyNumber = document.createElement("span");
-        frequencyNumber.classList.add("keyword-frequency");
-        li.innerText = value.name;
-        frequencyNumber.innerText = value.frequency;
-        li.append(frequencyNumber);
-        keywordsList.append(li);
-    }
 }
 
+function addToList(value) {
+    const li = document.createElement("li");
+    li.classList.add("keyword-item");
+    const frequencyNumber = document.createElement("span");
+    frequencyNumber.classList.add("keyword-frequency");
+    li.innerText = value.name;
+    frequencyNumber.innerText = value.frequency;
+    li.append(frequencyNumber);
+    keywordsList.append(li);
+}
 
 function removeKeywords() {
     while (keywordsList.firstElementChild)
@@ -47,7 +45,7 @@ function findKeywords() {
     makeFakeTextArea();
 
     itemList.forEach(keyword => {
-        let article = fakeTextarea.innerHTML;
+        const article = fakeTextarea.innerHTML;
 
         keyword.indexValue = createListOfIndex(keyword.name, article);
         keyword.frequency = keyword.indexValue.length;
@@ -56,49 +54,53 @@ function findKeywords() {
         markKeywords(article, keyword.name, keyword.indexValue);
     });
 
-    function makeFakeTextArea() {
-        seoContent.classList.add("hidden");
-        fakeTextarea.classList.remove("hidden");
-        fakeTextarea.textContent = seoContent.value;
+}
+
+function makeFakeTextArea() {
+    seoContent.classList.add("hidden");
+    fakeTextarea.classList.remove("hidden");
+    fakeTextarea.textContent = seoContent.value;
+}
+
+function createListOfIndex(value, source) {
+    const localIndexArray = [];
+    let index = -1;
+    const text = source.toLowerCase();
+    const valueText = value.toLowerCase();
+    while ((index = text.indexOf(valueText, index + 1)) >= 0) {
+        localIndexArray.push(index);
     }
+    return localIndexArray;
+}
 
-    function createListOfIndex (value, source) {
-        let localIndexArray = [];
-        let index = -1;
-        let text = source.toLowerCase();
-        let valueText = value.toLowerCase();
-        while ((index=text.indexOf(valueText, index + 1)) >= 0) {
-            localIndexArray.push(index);
-        }
-        return localIndexArray;
-    }
-
-    function updateFrequency(list) {
-        let listOfValue = document.querySelectorAll(".keyword-frequency");
-        for (let i = 0; i < listOfValue.length; i++) {
-            let keywordFrequency = listOfValue[i];
-            keywordFrequency.innerText = list[i].frequency;
-        }  
-    }
-
-    function markKeywords(text, keywordValue, arrayOfIndex) {
-
-        let markTagLength = 31;
-        let newArticle = text;
-
-        for(let i = 0; i < arrayOfIndex.length; i++) {
-            let startIndex = arrayOfIndex[i];
-
-            let firstPart = newArticle.slice(0, startIndex + (markTagLength * i));
-            let keywordText = newArticle.slice(startIndex + (markTagLength * i), (startIndex + (markTagLength * i)) + keywordValue.length);
-            let markPart = `<mark class="mark-word">${keywordText}</mark>`;
-            let endPart = newArticle.slice(startIndex + keywordValue.length + (markTagLength * i));
-            newArticle = firstPart + markPart + endPart; 
-        }
-
-        return fakeTextarea.innerHTML = newArticle;
+function updateFrequency(list) {
+    const listOfValue = document.querySelectorAll("#keywords-list li");
+    for (let i = 0; i < listOfValue.length; i++) {
+        const keywordFrequency = listOfValue[i];
+        keywordFrequency.innerText = list[i].frequency;
     }
 }
+
+function markKeywords(text, keywordValue, arrayOfIndex) {
+
+    const markTagLength = 31;
+    let newArticle = text;
+
+    // czy w warunku pętli też używać const i czy let i?
+
+    for (let i = 0; i < arrayOfIndex.length; i++) {
+        const startIndex = arrayOfIndex[i];
+
+        const firstPart = newArticle.slice(0, startIndex + (markTagLength * i));
+        const keywordText = newArticle.slice(startIndex + (markTagLength * i), (startIndex + (markTagLength * i)) + keywordValue.length);
+        const markPart = `<mark class="mark-word">${keywordText}</mark>`;
+        const endPart = newArticle.slice(startIndex + keywordValue.length + (markTagLength * i));
+        newArticle = firstPart + markPart + endPart;
+    }
+
+    return fakeTextarea.innerHTML = newArticle;
+}
+
 
 
 
